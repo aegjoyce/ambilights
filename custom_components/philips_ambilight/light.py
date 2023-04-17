fe14eee3e6b988cdbdc5e79690ce8d0978a50b43
aegjoyce <47081267+aegjoyce@users.noreply.github.com>
2023-04-17 20:55:28 +0100

    Update light.py

--- custom_components/philips_ambilight/light.py
+++ custom_components/philips_ambilight/light.py
@@ -125,12 +125,11 @@ class Ambilight(LightEntity):
         return True
 
     def turn_on(self, **kwargs):
-        state = self._state
-        if state == False:
-            self._postReq('ambilight/power', {'power':'On'})
-            time.sleep(0.1)
-
         if ATTR_HS_COLOR in kwargs:
+            state = self._state
+            if state == False:
+                self._postReq('ambilight/power', {'power':'On'})
+                time.sleep(0.1)
             self._hs = kwargs[ATTR_HS_COLOR]
             convertedHue = int(self._hs[0]*(255/360))
             convertedSaturation = int(self._hs[1]*(255/100))
@@ -141,19 +140,32 @@ class Ambilight(LightEntity):
             self._postReq('ambilight/lounge',{"color":{"hue":convertedHue,"saturation":convertedSaturation,"brightness":convertedBrightness},"colordelta":{"hue":0,"saturation":0,"brightness":0},"speed":0,"mode":"Default"} )
 
         elif ATTR_BRIGHTNESS in kwargs:
+            state = self._state
+            if state == False:
+                self._postReq('ambilight/power', {'power':'On'})
+                time.sleep(0.1)
             convertedBrightness = kwargs[ATTR_BRIGHTNESS]
             self._postReq('ambilight/lounge',{"color":{"hue":int(self._hs[0]*(255/360)),"saturation":int(self._hs[1]*(255/100)),"brightness":convertedBrightness},"colordelta":{"hue":0,"saturation":0,"brightness":0},"speed":0,"mode":"Default"} )
 
         elif ATTR_EFFECT in kwargs:
             effect = self._effect
             if effect == EFFECT_MANUAL:
-                self._postReq('ambilight/power', {'power':'Off'})
-                time.sleep(0.1)
+                state = self._state
+                if state == False:
+                    self._postReq('ambilight/power', {'power':'On'})
+                    time.sleep(0.1)
+                else:
+                    self._postReq('ambilight/power', {'power':'Off'})
+                    time.sleep(0.1)
             effect = kwargs[ATTR_EFFECT]
             self.set_effect(effect)
 
         else:
             if OLD_STATE[3] == EFFECT_MANUAL:
+                state = self._state
+                if state == False:
+                    self._postReq('ambilight/power', {'power':'On'})
+                    time.sleep(0.1)
                 self._postReq('ambilight/lounge',{"color":{"hue":int(OLD_STATE[0]*(255/360)),"saturation":int(OLD_STATE[1]*(255/100)),"brightness":OLD_STATE[2]},"colordelta":{"hue":0,"saturation":0,"brightness":0},"speed":0,"mode":"Default"} )
             else: 
                 effect = self._effect
